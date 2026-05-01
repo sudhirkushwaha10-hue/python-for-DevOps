ddb2f74c6734d8894b520be4278d4c5ffd6246cc
sudhirkushwaha10-hue <sudhirkushwaha10@gmail.com>
2026-05-01 17:50:23 +0530

    new update files

---
+++ casting.py
@@ -0,0 +1,20 @@
+# type casting
+# var1=float(1000)
+# print(type(var1))
+# print(var1)
+# var2= 1000
+# print(type(var2))
+# second condition of casting
+num1=50
+num2=20
+print(type(num1), type(num2))
+res=num1+num2
+print(str(num1)+str(num2))
+print(res)
+# third condition of casting
+num1="50"
+num2="20"
+print(type(num1), type(num2))
+res=num1+num2
+print(int(num1)+int(num2))
+print(res)
--- first.py
+++ first.py
@@ -9,3 +9,10 @@ print(c)
 print(d)
 print(e)
 print("sudhir")
+n=10
+m=10
+print(n+m)
+
+num1= "100"
+name= "dev"
+print (num1+name)
