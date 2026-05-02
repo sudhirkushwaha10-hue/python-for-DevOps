863fa63841575e40626ef557a99d30a4ac6a4122
sudhirkushwaha10-hue <sudhirkushwaha10@gmail.com>
2026-05-02 16:39:29 +0530

    add new files

---
+++ practicing.py
@@ -0,0 +1,23 @@
+name = "sudhir"
+age = 25
+price = 25.52
+print(type(name))
+print(type(age))
+print(type(price))
+print(int(price))
+a = None
+old = False
+print(type(a))
+print(type(old))
+
+
+
+a,b = 2,3
+c=4
+print(a+b*c)
+num = 20
+print(type(str(num)))
+print(type(float(num)))
+num2 = '20'
+print(type(int(num2)))
+print(type(num2))
