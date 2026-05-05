15e072adcc87af30d168a07b4e75a6098ff6bf90
sudhirkushwaha10-hue <sudhirkushwaha10@gmail.com>
2026-05-05 09:26:22 +0530

    main

--- practicing.py
+++ practicing.py
@@ -1,23 +1,76 @@
-name = "sudhir"
-age = 25
-price = 25.52
-print(type(name))
-print(type(age))
-print(type(price))
-print(int(price))
-a = None
-old = False
-print(type(a))
-print(type(old))
+# # # name = "sudhir"
+# # # age = 25
+# # # price = 25.52
+# # # print(type(name))
+# # # print(type(age))
+# # # print(type(price))
+# # # print(int(price))
+# # # a = None
+# # # old = False
+# # # print(type(a))
+# # # print(type(old))
 
 
 
-a,b = 2,3
-c=4
-print(a+b*c)
-num = 20
-print(type(str(num)))
-print(type(float(num)))
-num2 = '20'
-print(type(int(num2)))
-print(type(num2))
+# # # a,b = 2,3
+# # # c=4
+# # # print(a+b*c)
+# # # num = 20
+# # # print(type(str(num)))
+# # # print(type(float(num)))
+# # # num2 = '20'
+# # # print(type(int(num2)))
+# # # print(type(num2))
+
+
+
+# # # student_name= "sudhri verma"
+# # # student_address="s-195/31 giri nagar kalkaji"
+# # # student_number="8373949335"
+# # # print(len(student_number))
+# # # student_profile = f"hello my self this and i am {student_name} a devops intern looking for some challenging" 
+
+# # # print(student_profile)
+
+# # # operaters in python.
+# # # Arithmatic opr
+# # # 2. Assignment opr
+# # # 3. Comparision/relaional opr  (>,<,>=,<=,==,!=)
+# # # 4.logical opr ( and , or)
+# # # 5.Membershiop opr
+# # # 6. Identity opr
+# # # 7.Bitwise opr
+
+# # # Assignment opr
+# # a=10
+# # a=a+10 #standard method
+# # a+=10 #shorthand method
+# # a*=10 #shorthand method
+# # a=a*10 #standard mehtod
+# # print(a)
+# # n1=30
+# # n2=40
+# # # print(n1>n2)
+# # res1=n1>n2
+# # res2=n1<n2
+# # print(res1)
+# # print(res2)
+
+# # str1="A"
+# # str2="a"
+# # print(str1>str2)
+# # str1="Aman"
+# # str2="aman kumar"
+# # print(str1>str2)
+# # str1="aman"
+# # str2="Aman kumar"
+# # print(str1>str2)
+# str1="y"
+# str2="k"
+# print(str1>str2)
+
+n1=1
+n2=3
+res=not(n1==1 and n2==3 and n1==2 and n2==3)
+
+print(res)
