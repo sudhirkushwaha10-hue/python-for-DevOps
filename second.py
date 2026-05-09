f5d3e4dfecb91bc1524ca2bdcb99eb855bf94e6b
sudhirkushwaha10-hue <sudhirkushwaha10@gmail.com>
2026-05-09 11:36:00 +0530

    update

--- second.py
+++ second.py
@@ -1,10 +1,17 @@
-num1=2
-num2=15
-#sum1=num1+num2
+# num1=2
+# num2=15
+# #sum1=num1+num2
 
-#print(num1,"and",num2,"is equal",sum1)
-#print(f"{num1} and {num2} is equal {sum1}")
-power=num2**num1
-print(f"{num1}and{num2} = {power}")
-divide=num2%num1
-print(divide)
+# #print(num1,"and",num2,"is equal",sum1)
+# #print(f"{num1} and {num2} is equal {sum1}")
+# power=num2**num1
+# print(f"{num1}and{num2} = {power}")
+# divide=num2%num1
+# print(divide)
+
+a=str(5)
+b="dev"
+sum =b+a
+print(sum)
+
+
